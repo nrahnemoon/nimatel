bc07f89b0ec37c5bacb94847f977bf26b9e7b68a
Nima Rahnemoon <nrahnemoon@gmail.com>
2013-04-30 01:53:21 -0700

    Views.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Nimatel::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  scope :controller => :home do
+    match "/rates" => :rates, :as => :rates
+  end
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -48,7 +52,7 @@ Nimatel::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 

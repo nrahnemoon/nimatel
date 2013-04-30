b9569d35c628fde9605e7128aa8478a3c8f3f60f
Nima Rahnemoon <nrahnemoon@gmail.com>
2013-04-30 01:37:03 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nimatel::Application.config.secret_token = 'a890598d21a3bc2c87f2c6b3f435816d807d139689866fe45161237524f908e4826173759b9fd15229a3044a52582a3fea7e451c1b52654cf3189516f46f9998'
